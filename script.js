1015932b9379841ced84eee1f2ae6bdea5db9498
AnderG <agibarguen@gmail.com>
2022-08-15 10:02:45 +0200

    Structuring layout for the projects container 5

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function updateHour(){
         let hours = date.getHours()
         let minutes = date.getMinutes()
         let seconds = date.getSeconds()
+        function fixZeroes(time){
+            time= time.toString()
+            return time.length != 1 ? time : '0' + time
+        }
+        hours = fixZeroes(hours)
+        minutes = fixZeroes(minutes)
+        seconds = fixZeroes(seconds)
         return `${hours}:${minutes}:${seconds}`
     }
 }
@@ -32,34 +39,3 @@ function updateHour(){
 
 
 
-
-
-
-
-
-
-
-
-
-/*
-update()
-setInterval(update, 1000)
-function update(){
-    let date = new Date()
-    myHour.textContent = configureClock(date)
-    function configureClock(date){
-        let hours = date.getHours()
-        let minutes = date.getMinutes()
-        let seconds = date.getSeconds()
-        function fixZeroes(time){
-            time = time.toString()
-            return time.length<2? '0' + time : time
-        }
-        hours = fixZeroes(hours)
-        minutes = fixZeroes(minutes)
-        seconds = fixZeroes(seconds)
-
-        return `${hours}:${minutes}:${seconds} ${hours<12? 'am': 'pm'}`
-    }
-}
-*/
