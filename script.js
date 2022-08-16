3ef61716648cbd3c12e77856ae150531a53524da
AnderG <agibarguen@gmail.com>
2022-08-16 12:43:30 +0200

    Dom manipulation to add new divs into the project container 2

--- script.js
+++ script.js
@@ -22,9 +22,29 @@ function updateHour(){
 }
 
 //LIGHT MODE AND DARK MODE
+const checkbox = document.getElementById('checkbox')
+checkbox.addEventListener('click', ()=>{
+    document.querySelector('.user').classList.toggle('dark')
+    document.querySelector('.projects').classList.toggle('dark')
+    document.querySelector('.toogle').classList.toggle('dark')
+})
 
-
-
+//ADD DIV INTO THE PROJECT CONTAINER WHEN CLICKING NEW BUTTON
+const newBtnEl = document.getElementById('newBtn')
+const itemsContainer = document.querySelector('.projects-items')
+const itemEl = `<div class='item'>
+<p id="project-name">Project Name:</p>
+<p id="project-status"><img src="./Google Fonts/fiber_manual_record_FILL0_wght400_GRAD0_opsz48.svg">Status: Pending</p>
+<div>
+<img src="./Google Fonts/delete_FILL0_wght400_GRAD0_opsz48.svg" id="delete-img">
+<img src="./Google Fonts/edit_FILL0_wght400_GRAD0_opsz48.svg">
+</div>
+</div>`
+newBtnEl.addEventListener('click',addNewProject)
+function addNewProject(){
+  console.log('btn clicked')
+  itemsContainer.insertAdjacentHTML('beforebegin', itemEl)
+}
 
 
 
